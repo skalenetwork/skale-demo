d4d05d5787e4b28a41f45fa5497b21e32ffacd56
Christine <cperry7@gmail.com>
2019-03-25 14:47:10 -0700

    update web3 connector

--- make-a-cake/src/reducers/web3/getWeb3.js
+++ make-a-cake/src/reducers/web3/getWeb3.js
@@ -16,13 +16,7 @@ export let getWeb3 = new Promise(function(resolve, reject) {
   const web3Provider = new Web3.providers.HttpProvider(process.env.SKALE_CHAIN);
   var web3SKALE = new Web3(web3Provider);
 
-
-  const web3 = new Web3(new PortisProvider({
-   apiKey: 'bc85bd61-88da-4f5f-86df-21370919a269',
-   providerNodeUrl: "http://104.248.242.35:8014"
-  }));
-
-  console.log(web3);
+  const web3 = window.web3;
 
   let filestorage = new FilestorageClient(web3SKALE);
     
