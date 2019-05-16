0969f6ec86690aaf9fb8c09c5618a2c7edfdb6db
Christine <cperry7@gmail.com>
2019-05-16 16:43:34 -0400

    update formatting in block explorer

--- block-explorer/src/components/explorer/Explorer.js
+++ block-explorer/src/components/explorer/Explorer.js
@@ -130,11 +130,11 @@ class Explorer extends Component {
                 {block.transactionData ? block.transactionData.map((dataTx, index) => {
                   return (
                     <div key={index} data-id={index}>
-                      <button className={"list-group-item list-group-item-action blue-button border-between text-truncate " + ((index % 2) ? "background-grey" : "")} type="button" data-toggle="collapse" data-target={"#collapseDataTx_" + index} aria-expanded="true" aria-controls={"collapseDataTx_" + index}>
+                      <button className={"list-group-item list-group-item-action blue-button border-between text-truncate " + (filter ? ((index % 2) ? "background-grey" : "") : '')} type="button" data-toggle="collapse" data-target={"#collapseDataTx_" + index} aria-expanded="true" aria-controls={"collapseDataTx_" + index}>
                           {dataTx.hash}
                       </button>
 
-                      <div id={"collapseDataTx_" + index} className={"collapse transactionData pl-4 pt-1 pb-4 border-blue-top " + ((index % 2) ? "background-grey" : "")} aria-labelledby={"headingTx_" + index} data-parent="#dataTxBlock">
+                      <div id={"collapseDataTx_" + index} className={"collapse transactionData pl-4 pt-1 pb-4 border-blue-top " + (filter ? ((index % 2) ? "background-grey" : "") : '')} aria-labelledby={"headingTx_" + index} data-parent="#dataTxBlock">
                           <div><strong><span className="border-bottom border-dark">
                             Hash:</span></strong> {dataTx.hash}
                           </div>
